83def66ea532455d043edf3b74661949f7202b18
Illia Achour <ilyaachour@gmail.com>
2024-11-05 20:28:28 -0500

    Group dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,14 +40,22 @@ android {
 }
 
 dependencies {
+    // Android
     implementation(libs.androidx.core.ktx)
+
+    // Architecture
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // UI
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.splashscreen)
+
+    // Debug & test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
