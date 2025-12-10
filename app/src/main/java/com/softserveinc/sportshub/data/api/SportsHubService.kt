a942d464a60611dc1e1dbdf542abd11fd0dbb722
Illia Achour <iacho@softserveinc.com>
2025-12-10 03:55:10 -0500

    Update dependencies

--- app/src/main/java/com/softserveinc/sportshub/data/api/SportsHubService.kt
+++ app/src/main/java/com/softserveinc/sportshub/data/api/SportsHubService.kt
@@ -2,10 +2,21 @@ package com.softserveinc.sportshub.data.api
 
 import com.skydoves.sandwich.ApiResponse
 import com.softserveinc.sportshub.data.dto.ArticleDto
-import retrofit2.http.GET
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import javax.inject.Inject
 
-interface SportsHubService {
+class SportsHubService @Inject constructor(
+    private val httpClient: HttpClient
+) {
 
-    @GET("articles")
-    suspend fun getArticles(): ApiResponse<List<ArticleDto>>
+    suspend fun getArticles(): ApiResponse<List<ArticleDto>> {
+        return try {
+            val response = httpClient.get("articles")
+            ApiResponse.Success(response.body())
+        } catch (e: Exception) {
+            ApiResponse.error(e)
+        }
+    }
 }
